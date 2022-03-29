6fac216a100dfa13716491323ed86abe366019d1
yanis <chabaneyanis52@gmail.com>
2022-03-29 00:20:24 -0400

    av4

--- notes.py
+++ notes.py
@@ -1,10 +1,16 @@
 import pickle
+import easygui
 
 class notes:
+    openedfilebefore = False
+    fileopenned = None
     @staticmethod
     def notessaves(maintxt,sumtxt,headtxt,genekeys,adkeys): #sauvegarde le document
+        if notes.openedfilebefore ==False:
+            fileopened=easygui.filesavebox()
+            notes.fileopenned=fileopened
+            notes.openedfilebefore = True
         strgensv = ""
-
         for element in genekeys :
             strtemp=element.phrase
             strtemp += "@&&%*****"
@@ -17,8 +23,6 @@ class notes:
             strgensv+=strtemp
             strgensv+="@$?&"
         stradsv = ""
-
-
         for element in adkeys :
             strtemp = element.phrase
             strtemp += "@&&%*****"
@@ -33,9 +37,12 @@ class notes:
 
         txtsl=maintxt+'@&%*'+sumtxt+'@&%*'+headtxt+'@&%*'+strgensv+'@&%*'+stradsv#mets ensemble le tout pour optimiser la sauvegarde
          #  print(txtsl) #prinnt pour vérifier que ca fonctionne
-        pickle.dump(txtsl, open("savetest.bin", "wb")) #sauvegarde le document
+        pickle.dump(txtsl, open(notes.fileopenned, "wb")) #sauvegarde le document
     @staticmethod
     def notesload():
-        txtsl='hey'
-        txtsl = pickle.load(open(r"savetest.bin", "rb"))
+
+        filetoopen=easygui.fileopenbox()
+        notes.fileopenned=filetoopen
+        notes.openedfilebefore=True
+        txtsl = pickle.load(open(notes.fileopenned, "rb"))
         return txtsl
